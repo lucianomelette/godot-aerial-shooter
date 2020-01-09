389e2fd1113426355fb1bce3fb043a0ee03c3b98
Luciano Mariño <jmarino@falabella.com.ar>
2020-01-09 01:33:58 -0300

    Add parallax stars background

--- Main.gd
+++ Main.gd
@@ -4,23 +4,29 @@ extends Node
 # var a = 2
 # var b = "text"
 
+export var bg_stars_far_speed = 50
+export var bg_stars_close_speed = 150
+
+var screen_size = Vector2()
+var bg_height
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	screen_size = get_viewport().size
+	bg_height = $Stars_Far/Stars_Far_0.texture.get_height()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Sprite.position.y += 100 * delta
-	$Sprite2.position.y += 100 * delta
 	
-	if Input.is_action_pressed("ui_left"):
-		$Sprite.position.x += 30 * delta
-		$Sprite2.position.x += 30 * delta
-	if Input.is_action_pressed("ui_right"):
-		$Sprite.position.x -= 30 * delta
-		$Sprite2.position.x -= 30 * delta
+	# Background parallax effect
+	update_bg_position($Stars_Far/Stars_Far_0, $Stars_Far/Stars_Far_1, bg_stars_far_speed, delta)
+	update_bg_position($Stars_Far/Stars_Far_1, $Stars_Far/Stars_Far_0, bg_stars_far_speed, delta)
 	
-	if $Sprite.position.y > 610:
-		$Sprite.position.y = $Sprite2.position.y - 640
-	if $Sprite2.position.y > 610:
-		$Sprite2.position.y = $Sprite.position.y - 640
+	update_bg_position($Stars_Close/Stars_Close_0, $Stars_Close/Stars_Close_1, bg_stars_close_speed, delta)
+	update_bg_position($Stars_Close/Stars_Close_1, $Stars_Close/Stars_Close_0, bg_stars_close_speed, delta)
+		
+func update_bg_position(primary_bg, secondary_bg, speed, delta):
+	primary_bg.position.y += speed * delta
+	
+	if primary_bg.position.y > screen_size.y:
+		primary_bg.position.y = secondary_bg.position.y - bg_height
